1e218c01721ab356fa1daa2bff5462fe3bde583d
Ilya Braude <ilya@cloudmine.me>
2012-11-03 18:16:23 -0400

    integrated bottle and UI into styles

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,38 @@
-graph_data = [[0,1,], [1, 3]];
-graph = null;
+$(document).ready(function(){
 
-var socket = io.connect('http://localhost:8080');
-socket.on('reading', function (data) {
-//  console.log(data);
-  if(graph){
-    document.getElementById("reading").innerHTML = data.reading;
-    update(data.reading);
+  var graph_data = [[0,0]];
+  var graph;
+
+  var socket = io.connect('http://localhost:8080');
+  socket.on('reading', function (data) {
+    //  console.log(data);
+    if(graph){
+      document.getElementById("reading").innerHTML = data.reading;
+      update(data.reading);
+      $("#data_percent").css('height', (data.reading/10) + '%');
+    }
+  });
+
+  run();
+
+  function run() {
+    graph = new Dygraph(document.getElementById("div_g"), graph_data,
+                        {
+                          drawPoints: true,
+                          // showRoller: true,
+                          valueRange: [0.0, 1000],
+                          labels: ['Time', 'Weight']
+                        });
+  };
+
+  var counter = 0;
+  function update(reading){
+    //var x = new Date();  // current time
+    var x = counter++;
+    var y = reading;
+    graph_data.push([x, y]);
+    graph_data = graph_data.slice(-100); // truncate to 100 values
+
+    graph.updateOptions( { 'file': graph_data } );
   }
 });
-
-setTimeout(run, 1000);
-
-
-function run() {
-  graph = new Dygraph(document.getElementById("div_g"), graph_data,
-                      {
-                        drawPoints: true,
-                        showRoller: true,
-                        valueRange: [0.0, 1000],
-                        labels: ['Time', 'Weight']
-                      });
-};
-
-counter = 0;
-function update(reading){
-  //var x = new Date();  // current time
-  var x = counter++;
-  var y = reading;
-  graph_data.push([x, y]);
-  graph_data = graph_data.slice(-100); // truncate to 100 values
-
-  graph.updateOptions( { 'file': graph_data } );
-}
