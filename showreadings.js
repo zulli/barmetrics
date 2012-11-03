6d71cb0057953ac112b79877fcdad4a2ea1f5c4a
Kory Walton <korywalton@gmail.com>
2012-11-03 18:39:21 -0400

    made graph more visible

--- showreadings.js
+++ showreadings.js
@@ -5,7 +5,7 @@ var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 var express = require('express');
 
-var sp = new serialport.SerialPort("/dev/tty.usbmodemfa131", {
+var sp = new serialport.SerialPort("/dev/tty.usbmodemfd131", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
 });
